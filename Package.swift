e60918dd6e8399357b1559b353b31196c1d6f6c0
Ramadan Yassin <108692531+RamadanYassin@users.noreply.github.com>
2025-03-24 14:35:22 +0200

    Update Package.swift remove platform

--- Package.swift
+++ Package.swift
@@ -5,8 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "psdgen",
-    platforms: [
-    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
