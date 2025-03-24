410163bfdb86c86f1cfaca3b7a852ed52cec1cb3
Ramadan Yassin <108692531+RamadanYassin@users.noreply.github.com>
2025-03-24 14:31:13 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "psdgen",
     platforms: [
-        .macOS(.v15)
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
